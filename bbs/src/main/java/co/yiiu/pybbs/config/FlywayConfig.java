301b57b9a598b9a92668833ec1dfd6370e782587
foy.lian <ljs2342003@gmail.com>
2025-01-02 13:03:23 +0800

    init coin base

--- bbs/src/main/java/co/yiiu/pybbs/config/FlywayConfig.java
+++ bbs/src/main/java/co/yiiu/pybbs/config/FlywayConfig.java
@@ -1,6 +1,8 @@
 package co.yiiu.pybbs.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 
@@ -14,14 +16,22 @@ import javax.sql.DataSource;
  * https://atjiu.github.io
  */
 @Configuration
+@Slf4j
 public class FlywayConfig {
 
     @Resource
     private DataSource dataSource;
 
+    @Value("${spring.flyway.enabled:false}")
+    private boolean flyEnabled;
+
     @PostConstruct
     @DependsOn("dataSourceHelper")
     public void migrate() {
+        if(!flyEnabled){
+            log.info("disabled flyway migration.. skip db init.");
+            return;
+        }
         Flyway flyway = Flyway.configure().dataSource(dataSource).locations("classpath:db/migration",
                 "filesystem:db/migration").baselineOnMigrate(true).load();
         flyway.migrate();
