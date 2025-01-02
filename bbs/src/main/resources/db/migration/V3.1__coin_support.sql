fd8dbdb94cb17ec9c221adc253411832d8953ef5
foy.lian <ljs2342003@gmail.com>
2025-01-02 12:55:48 +0800

    init coin base

--- bbs/src/main/resources/db/migration/V3.1__coin_support.sql
+++ bbs/src/main/resources/db/migration/V3.1__coin_support.sql
@@ -9,7 +9,8 @@ CREATE TABLE `user_wallet`
     `locked_amount` decimal(40, 10)       default 0,
     `primary_address` varchar(255) NOT NULL,
     `encrypted_password`     varchar(255) DEFAULT '',
-    `encrypted_pub_idx_key ` varchar(255) DEFAULT '',
+    `encrypted_pub_idx_key` varchar(255) DEFAULT '',
+    PRIMARY KEY (`id`),
     UNIQUE KEY `wallet_key_unk` (`wallet_key`),
     UNIQUE KEY `user_name_coin_wallet_unk` (`username`,`coin_symbol`)
 )
@@ -17,11 +18,10 @@ CREATE TABLE `user_wallet`
 CREATE TABLE `user_coin_address`
 (
     `id`            int(11) NOT NULL AUTO_INCREMENT,
-    `label`         varchar(100)          DEFAULT '',
+    `label`         varchar(100) DEFAULT '',
     `wallet_key`    varchar(255) NOT NULL,
     `address`       varchar(255) NOT NULL,
-    PRIMARY KEY (`id`),
-    KEY             `username` (`username`)
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
