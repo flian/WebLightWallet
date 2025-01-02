e162559025b06713c373fda454576f5f6f1d0473
foy.lian <ljs2342003@gmail.com>
2025-01-02 13:19:36 +0800

    init coin base

--- bbs/src/main/resources/db/migration/V3.1__coin_support.sql
+++ bbs/src/main/resources/db/migration/V3.1__coin_support.sql
@@ -1,4 +1,5 @@
 
+DROP TABLE IF EXISTS `user_wallet`;
 CREATE TABLE `user_wallet`
 (
     `id`            int(11) NOT NULL AUTO_INCREMENT,
@@ -15,6 +16,7 @@ CREATE TABLE `user_wallet`
     UNIQUE KEY `user_name_coin_wallet_unk` (`username`,`coin_symbol`)
 )
 
+DROP TABLE IF EXISTS `user_coin_address`;
 CREATE TABLE `user_coin_address`
 (
     `id`            int(11) NOT NULL AUTO_INCREMENT,
@@ -24,7 +26,7 @@ CREATE TABLE `user_coin_address`
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
+DROP TABLE IF EXISTS `rsa_private_pub_key`;
 CREATE TABLE `rsa_private_pub_key`
 (
     `id` int(11) NOT NULL AUTO_INCREMENT,
